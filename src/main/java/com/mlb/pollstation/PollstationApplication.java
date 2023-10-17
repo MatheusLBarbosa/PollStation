20d2d7782d9d1d9ebd208f8d2d1b8a696f1f93fb
MatheusLBarbosa <barbosa.matheuslopes@gmail.com>
2023-10-16 21:19:03 -0300

    initial commit

---
+++ src/main/java/com/mlb/pollstation/PollstationApplication.java
@@ -0,0 +1,13 @@
+package com.mlb.pollstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollstationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mlb/pollstation/PollstationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlb.pollstation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollstationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
