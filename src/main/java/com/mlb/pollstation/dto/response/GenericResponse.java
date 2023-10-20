5c8175e25a4f495e92d05063a87d0112c4435faa
MatheusLBarbosa <barbosa.matheuslopes@gmail.com>
2023-10-20 08:49:39 -0300

    create GeneralException, and add more properties to .properties file

---
+++ src/main/java/com/mlb/pollstation/dto/response/GenericResponse.java
@@ -0,0 +1,11 @@
+package com.mlb.pollstation.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class GenericResponse {
+    private String message;
+    private Integer status;
+}
